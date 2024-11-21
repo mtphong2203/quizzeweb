cfaf5161410abbbf83f976620079873f9eb0bab8
Tien Phong <tienphong2203@gmail.com>
2024-11-21 09:06:36 +0700

    Step 1: Create base project

---
+++ src/main/java/com/maiphong/quizzeweb/QuizzewebApplication.java
@@ -0,0 +1,13 @@
+package com.maiphong.quizzeweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizzewebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizzewebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maiphong/quizzeweb/QuizzewebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maiphong.quizzeweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizzewebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
